6afcdbdbed36c4ba4137d7bb40c04f42cab1a81b
Noah Lanson <lanson.noah@gmail.com>
2021-09-10 22:04:21 +1000

    Hash tests for byte arrays and strings of public keys.

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,7 @@ pub use bitcoin_hashes::{sha256, ripemd160};
 
 #[cfg(test)]
 mod tests {
-    use secp256k1::{PublicKey, Secp256k1, SecretKey};
+    use sha2::{Sha256, Digest}; //Temporary
 
     use crate::key::{
         PrivKey, PubKey
@@ -27,10 +27,42 @@ mod tests {
     
     #[test]
     fn keys_test() {
-        let secp = Secp256k1::new();
         let k: PrivKey = PrivKey::new_rand();
         let pk: PubKey = PubKey::from_priv_key(&k);
 
         assert_eq!(pk.0, PubKey::from_priv_key(&k).0);
+    }
+
+    //This test was written to check if a uncompressed public key in form [u8; 65] can be hashed
+    //similarly to a compressed public key in form string. 
+    #[test]
+    fn hash_identical_test() {
+        let k: PrivKey = PrivKey::new_rand();
+        let pk: PubKey = PubKey::from_priv_key(&k);
+        
+
+        //String from serialized.
+        let serialised = pk.0.serialize();
+        let s = serialised.iter().map(|x| format!("{:02x}", x)).collect::<String>();
+
+        //String from mehtod
+        let a = pk.0.to_string();
+
+        //Should be equal
+        assert_eq!(s, a);
+
+
+        //Hash of serialised to string
+        let mut hasher1 = Sha256::new();
+        hasher1.update(a);
+        let result1 = hasher1.finalize();
+        
+        //Hash of string method
+        let mut hasher2 = Sha256::new();
+        hasher2.update(s);
+        let result2 = hasher2.finalize();
+
+        //Should be equal
+        assert_eq!(result1, result2);
     }
 }
